9bdad5cb82aef1e18ce57600ee20070e38ff5243
Abhay <abys7315@gmail.com>
2025-12-29 09:35:20 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,6 @@ app.use("/api/marks", require("./routes/marks"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 
 // Serve static files from the React app build directory
-app.use(express.static(path.join(process.cwd(), "grade-calculator-frontend", "dist")));
-
-// Catch all handler: send back React's index.html file for client-side routing
-app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(process.cwd(), "grade-calculator-frontend", "dist", "index.html"));
-});
 
 app.listen(process.env.PORT, () =>
   console.log("Server running on", process.env.PORT)
