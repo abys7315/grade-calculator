0a0940611d40ea498de3ec1ebff8214131f60e26
Abhay <abys7315@gmail.com>
2025-12-30 01:10:56 +0530

    add server

--- grade-calculator-frontend/src/api/axios.js
+++ grade-calculator-frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"  
+  baseURL: "https://grade-calculator-pjm2.onrender.com/api" || "http://localhost:5000/api"  
 });
-// "https://grade-calculator-pjm2.onrender.com/api" || "
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
