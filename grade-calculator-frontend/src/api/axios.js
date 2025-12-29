323247e19ae0fdb14ee3e7677bc75bb1306a3641
Abhay <abys7315@gmail.com>
2025-12-30 01:28:15 +0530

    authentication

--- grade-calculator-frontend/src/api/axios.js
+++ grade-calculator-frontend/src/api/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://grade-calculator-pjm2.onrender.com/api" || "http://localhost:5000/api"  
+  baseURL:"https://grade-calculator-pjm2.onrender.com/api" ||  "http://localhost:5000/api"  
 });
-
+// 
 instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
