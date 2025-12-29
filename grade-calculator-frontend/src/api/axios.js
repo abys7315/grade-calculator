70c398e31851ce430d3373d4f084acf4386cfd48
Abhay <abys7315@gmail.com>
2025-12-29 10:19:55 +0530

    axios

--- grade-calculator-frontend/src/api/axios.js
+++ grade-calculator-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api" || "https://grade-calculator-pjm2.onrender.com/api"
+  baseURL:  "https://grade-calculator-pjm2.onrender.com/api" || "http://localhost:5000/api"
 });
 
 instance.interceptors.request.use((config) => {
