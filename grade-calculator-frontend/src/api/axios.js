293b087d4ad501cf54debad6bd3a080d45aad972
Abhay <abys7315@gmail.com>
2025-12-29 07:27:38 +0530

    update backend url

--- grade-calculator-frontend/src/api/axios.js
+++ grade-calculator-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://localhost:5000/api" || "https://grade-calculator-pjm2.onrender.com/api"
 });
 
 instance.interceptors.request.use((config) => {
